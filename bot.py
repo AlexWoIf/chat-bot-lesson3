2c9b7ebe8271f795e8ac401907ba43ecaa442092
AlexWoIf <alexey.barannikov@gmail.com>
2024-01-15 12:26:05 +0300

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -1,16 +1,25 @@
 import os
+import logging
 
 from dotenv import load_dotenv
 from telegram.ext import Updater, MessageHandler, Filters
 
+from dialog_flow import detect_intent_texts
 
+logger = logging.getLogger(__name__)
 def echo(update, context):
-    update.message.reply_text(update.message.text)
+    text = update.message.text
+    session = update.effective_chat.id
+    answer = detect_intent_texts(project_id, session, text, 'ru')
+    update.message.reply_text(answer)
 
 
 if __name__ == '__main__':
     load_dotenv()
+    api_key = os.getenv('GOOGLE_CLOUD_API_KEY')
+    project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
     tg_token = os.getenv("TELEGRAM_BOT_TOKEN")
+
     updater = Updater(tg_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(MessageHandler(Filters.text, echo))
---
+++ dialog_flow.py
@@ -0,0 +1,42 @@
+
+import logging
+import os
+
+from dotenv import load_dotenv
+from google.cloud import dialogflow
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    logger.info("Session path: {}\n".format(session))
+
+    text_input = dialogflow.TextInput(text=text,
+                                      language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    logger.info(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+    return response.query_result.fulfillment_text
